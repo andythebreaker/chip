6072b6acb474fe75f800c35558fbfd76e25f1e06
andythebreaker <e24074724@mail.ncku.edu.tw>
2023-05-20 14:20:19 +0800

    tool b update

--- tool/matrix_vector_times_b.js
+++ tool/matrix_vector_times_b.js
@@ -10,9 +10,10 @@ var hedder_4 = hedder_0 + '\n' + hedder_1 + String(matrix_size_1) + '\n' + hedde
 var hedder_5 = `module B_matrix_times_vector_genED_M_${matrix_size_1}_${matrix_size_2}_V_${matrix_size_1} (`;
 var hedder_6 = 'input logic signed [31:0] matrix[0:`size_minus_1(`matrix_size_1)][0:`size_minus_1(`matrix_size_2)],';
 var hedder_7 = 'input logic signed [31:0] vector[0:`size_minus_1(`vector_size)],';
+var hedder_7_5 = 'input logic signed [31:0] b[0:`size_minus_1(`matrix_size_2)],';
 var hedder_8 = 'output logic signed [31:0] result[0:`size_minus_1(`matrix_size_2)]';
 var hedder_9 = ');';
-var hedder_10 = hedder_4 + '\n' + hedder_5 + '\n' + hedder_6 + '\n' + hedder_7 + '\n' + hedder_8 + '\n' + hedder_9 + '\n';
+var hedder_10 = hedder_4 + '\n' + hedder_5 + '\n' + hedder_6 + '\n' + hedder_7 + '\n'+hedder_7_5+'\n' + hedder_8 + '\n' + hedder_9 + '\n';
 fs.appendFileSync(`B_matrix_times_vector_genED_M_${matrix_size_1}_${matrix_size_2}_V_${matrix_size_1}.sv`, hedder_10, (err) => {
     if (err) {
         console.error(err);
