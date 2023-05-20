c07d95f99b0b5362d6883fae4788c8526c2a42d0
andythebreaker <43373581+andythebreaker@users.noreply.github.com>
2023-05-20 23:21:50 +0800

    Update tanh.sv

--- tanh.sv
+++ tanh.sv
@@ -10,10 +10,10 @@ module tanh (input logic signed [31:0] x,
             y = x;
             end else if (z >= (0.5 * 10**8) && z <= (1.2 * 10**8)) begin
             //$display("%d case2",z);
-            y = (x /2) + (0.25 * 10**8);
+            y = (x /2) + (0.25 * 10**8)*((x>= 0)?1:-1);
             end else if (z >= (1.2 * 10**8) && z <= (2.4 * 10**8)) begin
             //$display("%d case3",z);
-            y = (x /8) + (0.7 * 10**8);
+            y = (x /8) + (0.7 * 10**8)*((x>= 0)?1:-1);
             end else if (x >= (2.4 * 10**8) && x >= 0) begin
             //$display("%d case4",z);
             y = 32'h7FFFFFFF;
