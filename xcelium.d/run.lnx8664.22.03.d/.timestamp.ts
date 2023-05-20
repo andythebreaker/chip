39de551169dadef2e53bc57ce4fc51f585e8afa0
andythebreaker <e24074724@mail.ncku.edu.tw>
2023-05-20 17:32:48 +0800

    verilog error based number lacks value

--- xcelium.d/run.lnx8664.22.03.d/.timestamp.ts
+++ xcelium.d/run.lnx8664.22.03.d/.timestamp.ts
@@ -1,10 +1,14 @@
 1684541132 /home/AOC2023/aoc2023_036/code/andy_chip/matmux_f.sv
 1684544887 /home/AOC2023/aoc2023_036/code/andy_chip/matmux_32_tb.sv
+1684565890 /home/AOC2023/aoc2023_036/code/chip/tool/B_matrix_times_vector_genED_M_100_400_V_100.sv
 1684554978 /home/AOC2023/aoc2023_036/code/chip/matmux_35_tb.sv
 1684540907 /home/AOC2023/aoc2023_036/code/andy_chip/matmux_f_tb.sv
 1684557568 /home/AOC2023/aoc2023_036/code/chip/matmux_36_tb.sv
 1684560190 /home/AOC2023/aoc2023_036/code/chip/matmux_37_tb.sv
+1684559585 /home/AOC2023/aoc2023_036/code/chip/tool/matrix_times_vector_genED_M_100_400_V_100.sv
 1684546020 /home/AOC2023/aoc2023_036/code/andy_chip/matmux_35_tb.sv
 1684541490 /home/AOC2023/aoc2023_036/code/andy_chip/fa.sv
 1684560783 /home/AOC2023/aoc2023_036/code/chip/matmux_32.sv
+1684570967 /home/AOC2023/aoc2023_036/code/chip/vmvmb.sv
+1684560895 /home/AOC2023/aoc2023_036/code/chip/tool/vector_add_vector_genED_M_100_400_V_100.sv
 1684546381 /home/AOC2023/aoc2023_036/code/andy_chip/matmux_32.sv
