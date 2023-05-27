4acea232366cf6644c61d49b32d4a6df308b74c7
andythebreaker <e24074724@mail.ncku.edu.tw>
2023-05-27 20:04:33 +0800

    adj_t

--- tanh_module_workspace/tanh.sv
+++ tanh_module_workspace/tanh.sv
@@ -1,3 +1,4 @@
+`define adj_t 21
 module tanh (input logic signed [31:0] x,
              output logic signed [31:0] y);
     
@@ -7,13 +8,13 @@ module tanh (input logic signed [31:0] x,
         z = (x>= 0)?x:(32'd0-x);
         if (z <= (0.5 * 10**8)) begin
             //$display("%d case1",z);
-            y = x;
+            y = x*adj_t;
             end else if (z >= (0.5 * 10**8) && z <= (1.2 * 10**8)) begin
             //$display("%d case2",z);
-            y = (x /2) + (0.25 * 10**8)*((x>= 0)?1:-1);
+            y = ((x /2) + (0.25 * 10**8)*((x>= 0)?1:-1))*adj_t;
             end else if (z >= (1.2 * 10**8) && z <= (2.4 * 10**8)) begin
             //$display("%d case3",z);
-            y = (x /8) + (0.7 * 10**8)*((x>= 0)?1:-1);
+            y = ((x /8) + (0.7 * 10**8)*((x>= 0)?1:-1))*adj_t;
             end else if (x >= (2.4 * 10**8) && x >= 0) begin
             //$display("%d case4",z);
             y = 32'h7FFFFFFF;
