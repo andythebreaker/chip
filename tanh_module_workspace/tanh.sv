4ce44d6a02f771e61347ba6edc08f9a21ea418d9
andythebreaker <e24074724@mail.ncku.edu.tw>
2023-05-27 22:09:01 +0800

    https://github.com/andythebreaker/chip/assets/43373581/b030d8bb-3928-4351-a3e3-01da02e7a456

--- tanh_module_workspace/tanh.sv
+++ tanh_module_workspace/tanh.sv
@@ -8,13 +8,13 @@ module tanh (input logic signed [31:0] x,
         z = (x>= 0)?x:(32'd0-x);
         if (z <= (0.5 * 10**8)) begin
             //$display("%d case1",z);
-            y = x*adj_t;
+            y = x*`adj_t;
             end else if (z >= (0.5 * 10**8) && z <= (1.2 * 10**8)) begin
             //$display("%d case2",z);
-            y = ((x /2) + (0.25 * 10**8)*((x>= 0)?1:-1))*adj_t;
+            y = ((x /2) + (0.25 * 10**8)*((x>= 0)?1:-1))*`adj_t;
             end else if (z >= (1.2 * 10**8) && z <= (2.4 * 10**8)) begin
             //$display("%d case3",z);
-            y = ((x /8) + (0.7 * 10**8)*((x>= 0)?1:-1))*adj_t;
+            y = ((x /8) + (0.7 * 10**8)*((x>= 0)?1:-1))*`adj_t;
             end else if (x >= (2.4 * 10**8) && x >= 0) begin
             //$display("%d case4",z);
             y = 32'h7FFFFFFF;
