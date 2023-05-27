4acba6d68c9b7421e7e05a79c21493df5ce545fb
andythebreaker <e24074724@mail.ncku.edu.tw>
2023-05-27 21:12:21 +0800

    logspace test

---
+++ tanh_module_workspace/checking_data/logspace_test.js
@@ -0,0 +1,19 @@
+const startPoint = -2147483648;
+const endPoint = 2147483647;
+const numPoints = 10000;
+
+const logspacePoints = [];
+const logStart = Math.log10(startPoint);
+const logEnd = Math.log10(endPoint);
+const step = (logEnd - logStart) / (numPoints - 1);
+
+for (let i = 0; i < numPoints; i++) {
+  const logValue = logStart + i * step;
+  const value = Math.pow(10, logValue);
+  logspacePoints.push(value);
+}
+
+// Print the points
+for (let i = 0; i < logspacePoints.length; i++) {
+  console.log(`#10;x=${logspacePoints[i]};`);
+}
