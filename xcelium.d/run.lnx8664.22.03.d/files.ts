c8dcf996011711f10e2c801be8d970d11dc3b845
andythebreaker <e24074724@mail.ncku.edu.tw>
2023-05-29 16:00:34 +0800

    模擬

--- xcelium.d/run.lnx8664.22.03.d/files.ts
+++ xcelium.d/run.lnx8664.22.03.d/files.ts
@@ -1,1 +1,2 @@
 1684680457 /home/AOC2023/aoc2023_036/git/chip/tool/B_matrix_times_vector_genED_M_100_400_V_100.sv
+1684680457 /home/AOC2023/aoc2023_036/git/chip/tool/matrix_times_vector_genED_M_100_400_V_100.sv
