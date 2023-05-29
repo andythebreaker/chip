1e49695174e4a0e4b669acd23bfacacdfaf76195
andythebreaker <e24074724@mail.ncku.edu.tw>
2023-05-29 17:07:59 +0800

    要去做v*v小龜測

--- tool/vector_vector_mul.js
+++ tool/vector_vector_mul.js
@@ -3,8 +3,8 @@ const fs = require('fs');
 var max_int32 = 2147483647;
 var min_int32 = -2147483648;
 
-var matrix_size_1 = 100;
-var matrix_size_2 = 400;
+var matrix_size_1 = 5;//100
+var matrix_size_2 = 400;//400
 var hedder_0 = '`define size_minus_1(x) (x-1)';
 var hedder_1 = '`define matrix_size_1 ';
 var hedder_2 = '`define matrix_size_2 ';
