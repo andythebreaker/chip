e11370612f6399ebf16bfc96998a608583c21391
andythebreaker <e24074724@mail.ncku.edu.tw>
2023-05-29 16:51:19 +0800

    test

---
+++ test/t1.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+# a var shape (1, 5)
+a = np.array([[1, 2, 3, 4, 5]])
+# print a shape
+print(a.shape)
+# a var shape (1, 5)
+b = np.array([[6, 7, 8, 9, 10]])
+# print a shape
+print(b.shape)
+# a*b
+c = a * b
+# print result
+print(c)
