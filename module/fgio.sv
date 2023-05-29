fe4cdaf9ddc0857e5a6b5dc05010e823d194dee4
andythebreaker <e24074724@mail.ncku.edu.tw>
2023-05-29 20:22:17 +0800

    fgio pg...

--- module/fgio.sv
+++ module/fgio.sv
@@ -3,8 +3,10 @@
 `define matrix_size_2 400
 `define vector_size `matrix_size_1
 
-`include "../sigmoid_module_workspace/sigmoid.sv"
-`include "../tanh_module_workspace/tanh.sv"
+//`include "../sigmoid_module_workspace/sigmoid.sv"
+//`include "../tanh_module_workspace/tanh.sv"
+`include "./long_non_linear_shape_100_400_sigmoid.sv"
+`include "./long_non_linear_shape_100_400_tanh.sv"
 
 module fgio (
 input logic signed [31:0] c_prev[0:`size_minus_1(`vector_size)],
@@ -17,6 +19,16 @@ output logic signed [31:0] c_next[0:`size_minus_1(`vector_size)],
 output logic signed [31:0] h_t[0:`size_minus_1(`vector_size)]
 );
 
-sigmoid sigmoid_f()
+// define non-linear layer
+logic signed [31:0] f[0:`size_minus_1(`vector_size)];
+logic signed [31:0] g[0:`size_minus_1(`vector_size)];
+logic signed [31:0] i[0:`size_minus_1(`vector_size)];
+logic signed [31:0] o[0:`size_minus_1(`vector_size)];
+
+// connect non-linear layer
+long_non_linear_shape_100_400_sigmoid nl_f(fgio_in[0:`size_minus_1(100)],f);
+long_non_linear_shape_100_400_tanh nl_g(fgio_in[0:`size_minus_1(100)],g);
+long_non_linear_shape_100_400_sigmoid nl_i(fgio_in[0:`size_minus_1(100)],i);
+long_non_linear_shape_100_400_sigmoid nl_o(fgio_in[0:`size_minus_1(100)],o);
     
 endmodule
