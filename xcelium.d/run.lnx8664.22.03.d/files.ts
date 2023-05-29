314b50e60ebb012586bc96d2ed5b2ac5102dd44e
andythebreaker <e24074724@mail.ncku.edu.tw>
2023-05-29 15:59:54 +0800

    整理

--- xcelium.d/run.lnx8664.22.03.d/files.ts
+++ xcelium.d/run.lnx8664.22.03.d/files.ts
@@ -1,2 +1,1 @@
-1684680457 /home/AOC2023/aoc2023_036/git/chip/tanh.sv
-1684680457 /home/AOC2023/aoc2023_036/git/chip/tanh_tb.sv
+1684680457 /home/AOC2023/aoc2023_036/git/chip/tool/B_matrix_times_vector_genED_M_100_400_V_100.sv
